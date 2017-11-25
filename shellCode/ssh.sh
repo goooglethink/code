785eb92ed635bb7b6d58042957f29adf90bb0217
googlethink <goooglethink@gmail.com>
2017-11-25 00:35:14 -0500

    update

--- shellCode/ssh.sh
+++ shellCode/ssh.sh
@@ -2,15 +2,17 @@
 ##############################################
 #Author: GoogleThink
 #Mail:goooglethink@gmail.com
-#Last modified: 2017-10-12 07:45
-#Filename: suroot.sh
+#Last modified: 2017-10-12 07:41
+#Filename: ssh.sh
 #Description:
 ##############################################
+
 #!/bin/bash
 expect -c "  
-spawn su -  
-expect \":\"  
-send \"google\r\"  
-interact  
+spawn ssh -l root 163.44.119.41
+expect "password:"  
+send \"QQqq0.123456789\r\"  
+interact 
 " 
 
+#sudo ssh root@150.95.181.233
